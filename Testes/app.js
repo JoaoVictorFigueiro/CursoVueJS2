f107d4cf6f0e0ac0c0b72e5fcb0092b2406fd939
João Victor <joviooli@gmail.com>
2022-09-27 20:13:49 -0300

    Teste_1

---
+++ Testes/app.js
@@ -0,0 +1,17 @@
+new Vue ({
+    el: '#desafio',
+    data: {
+        nome: 'João Victor',
+        idade: 22,
+        imagem: 'https://images7.alphacoders.com/633/633262.png'
+    },
+
+    methods: {
+        random() {
+         return Math.random() 
+        }
+        
+    }
+    
+
+})
